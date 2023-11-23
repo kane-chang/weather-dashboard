18492fcf001b282ab0ecf1d5ade4d3b0741e2d3b
Kane Chang <kane.chang.fl@gmail.com>
2023-11-23 21:52:59 +0000

    added TODO comments

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// TODO add API and query fetch calls
+
+// TODO fetch geocoding API
+
+// TODO fetch 5 day weather forecast API
+
+// TODO search eventlistener to pull API data
+
+// TODO format and create city weather data HTML elements
+
+// TODO format and create forecast data HTML elements
+
+// TODO create a button function with every searched city
+
+// TODO store buttons and their data to local storage
