4ca5f1d53f49df938f50f3704c5caf403d6dd38f
Kane Chang <kane.chang.fl@gmail.com>
2023-11-24 17:58:26 +0000

    added createTodaySection function to generate today's weather html elements

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ async function fetchGeocodeForecast(event) {
             // var lon = -0.1180
             var lat = (data[0].lat);
             var lon = (data[0].lon);
-            var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=a95b437b4c6c849e80cc1cc3ab83452a"
+            var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=metric&appid=a95b437b4c6c849e80cc1cc3ab83452a"
             return forecastQuery
         })
     fetch(forecastQ)
@@ -24,6 +24,7 @@ async function fetchGeocodeForecast(event) {
         })
         .then(function (data) {
             console.log(data);
+            createTodaySection(data);
         })
 }
 
@@ -31,11 +32,41 @@ async function fetchGeocodeForecast(event) {
 // TODO search eventlistener to pull API data
 var searchButton = document.querySelector('#search-button')
 var searchInput = document.querySelector('#search-input')
+var todaySection = document.querySelector('#today')
+var forecastSection = document.querySelector('#forecast')
+
 searchButton.addEventListener("click", fetchGeocodeForecast)
 
-// TODO format and create city weather data HTML elements
+// TODO format and create today's city weather data HTML elements
+function createTodaySection(data) {
+    var cityHeader = document.createElement('h2')
+    var tempText = document.createElement('p')
+    var windText = document.createElement('p')
+    var humidText = document.createElement('p')
+    var weatherIcon = document.createElement('img')
 
-// TODO format and create forecast data HTML elements
+    var todayDate = dayjs().format('(DD/MM/YYYY)')
+    var iconCode = (data.list[0].weather[0].icon);
+    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+    weatherIcon.setAttribute('src', iconUrl)
+
+    cityHeader.textContent = data.city.name + " " + todayDate + " "
+    cityHeader.append(weatherIcon)
+    tempText.textContent = "Temp: " + data.list[0].main.temp + "°C"
+    windText.textContent = "Wind Speed: " + data.list[0].wind.speed + " KPH"
+    humidText.textContent = "Humidity: " + data.list[0].main.humidity +"%"
+
+    todaySection.append(cityHeader, tempText, windText, humidText)
+}
+
+// TODO format and create 5-day forecast data HTML elements
+function createForecastSection(data) {
+    var secondsInADay = 86400
+    var 
+
+}
+
+
 
 // TODO create a button function with every searched city
 
