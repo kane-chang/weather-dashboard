a11dc4ae6d798ab0ebe85549d01a5a761a3020a8
Kane Chang <kane.chang.fl@gmail.com>
2023-11-27 11:45:40 +0000

    added createCityButton function

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ async function fetchGeocodeForecast(event) {
         .then(function (data) {
             if (data[0] == undefined) {
                 console.log('oops');
-                errorMessage()
+                errorMessage();
             } else {
                 console.log(data[0]);
                 var lat = (data[0].lat);
@@ -30,9 +30,10 @@ async function fetchGeocodeForecast(event) {
                 console.log(data);
                 createTodaySection(data);
                 createForecastSection(data);
+                createCityButton(cityName);
             })
     }
-}
+};
 
 // TODO error message function if search fails
 function errorMessage() {
@@ -47,16 +48,7 @@ function errorMessage() {
             clearInterval(feedbackTimeInterval);
         };
     }, 1000);
-}
-
-// TODO search eventlistener to pull API data
-var searchButton = document.querySelector('#search-button')
-var searchInput = document.querySelector('#search-input')
-var searchDiv = document.querySelector('#input-div')
-var todaySection = document.querySelector('#today')
-var forecastSection = document.querySelector('#forecast')
-
-searchButton.addEventListener("click", fetchGeocodeForecast)
+};
 
 // TODO format and create today's city weather data HTML elements
 function createTodaySection(data) {
@@ -84,64 +76,82 @@ function createTodaySection(data) {
     humidText.textContent = "Humidity: " + data.list[0].main.humidity + "%"
 
     todaySection.append(cityHeader, tempText, windText, humidText)
-}
+};
 
 // TODO format and create 5-day forecast data HTML elements
 function createForecastSection(data) {
     if (forecastSection.children.length > 0) {
         while (forecastSection.firstChild) {
             forecastSection.removeChild(forecastSection.lastChild);        
-        }
-    }
-    var secondsInADay = 86400
-    var fiveDayArray = [data.list[0].dt + secondsInADay, data.list[0].dt + secondsInADay * 2, data.list[0].dt + secondsInADay * 3, data.list[0].dt + secondsInADay * 4, data.list[0].dt + secondsInADay * 5 - 10800]
-    var forecastHeader = document.createElement('h3')
-    var rowDiv = document.createElement('div')
-    forecastHeader.textContent = "5-Day Forecast:"
+        };
+    };
+    var secondsInADay = 86400;
+    var fiveDayArray = [data.list[0].dt + secondsInADay, data.list[0].dt + secondsInADay * 2, data.list[0].dt + secondsInADay * 3, data.list[0].dt + secondsInADay * 4, data.list[0].dt + secondsInADay * 5 - 10800];
+    var forecastHeader = document.createElement('h3');
+    var rowDiv = document.createElement('div');
+    forecastHeader.textContent = "5-Day Forecast:";
     console.log(fiveDayArray);
 
-    forecastSection.append(forecastHeader, rowDiv)
+    forecastSection.append(forecastHeader, rowDiv);
 
     for (let j = 0; j < fiveDayArray.length; j++) {
         for (let i = 0; i < data.list.length; i++) {
             if (data.list[i].dt == fiveDayArray[j]) {
                 // Card HTML elements
-                var weatherCard = document.createElement('div')
-                var cardBodyDiv = document.createElement('div')
-                var cardTitle = document.createElement('h5')
-                var cardIcon = document.createElement('img')
-                var cardTemp = document.createElement('p')
-                var cardWind = document.createElement('p')
-                var cardHumid = document.createElement('p')
+                var weatherCard = document.createElement('div');
+                var cardBodyDiv = document.createElement('div');
+                var cardTitle = document.createElement('h5');
+                var cardIcon = document.createElement('img');
+                var cardTemp = document.createElement('p');
+                var cardWind = document.createElement('p');
+                var cardHumid = document.createElement('p');
 
-                rowDiv.setAttribute('class', 'row')
-                weatherCard.setAttribute('class', 'card text-white bg-dark col-sm mx-3')
-                cardBodyDiv.setAttribute('class', 'card-body')
-                cardTitle.setAttribute('class', 'card-title')
+                rowDiv.setAttribute('class', 'row');
+                weatherCard.setAttribute('class', 'card text-white bg-dark col-sm mx-3');
+                cardBodyDiv.setAttribute('class', 'card-body');
+                cardTitle.setAttribute('class', 'card-title');
                 console.log(fiveDayArray[j]);
                 console.log(data.list[i].dt_txt);
-                cardDate = dayjs().add(j + 1, 'day').format('DD/MM/YYYY')
+                cardDate = dayjs().add(j + 1, 'day').format('DD/MM/YYYY');
                 console.log(cardDate);
-                cardTitle.textContent = cardDate
+                cardTitle.textContent = cardDate;
 
                 var cardIconCode = (data.list[i].weather[0].icon);
-                var cardIconUrl = "http://openweathermap.org/img/w/" + cardIconCode + ".png"
-                cardIcon.setAttribute('src', cardIconUrl)
+                var cardIconUrl = "http://openweathermap.org/img/w/" + cardIconCode + ".png";
+                cardIcon.setAttribute('src', cardIconUrl);
 
-                cardTemp.textContent = "Temp: " + data.list[i].main.temp + "°C"
-                cardWind.textContent = "Wind: " + data.list[i].wind.speed + " KPH"
-                cardHumid.textContent = "Humidity: " + data.list[i].main.humidity + "%"
+                cardTemp.textContent = "Temp: " + data.list[i].main.temp + "°C";
+                cardWind.textContent = "Wind: " + data.list[i].wind.speed + " KPH";
+                cardHumid.textContent = "Humidity: " + data.list[i].main.humidity + "%";
 
-                cardBodyDiv.append(cardTitle, cardIcon, cardTemp, cardWind, cardHumid)
-                weatherCard.appendChild(cardBodyDiv)
-                rowDiv.appendChild(weatherCard)
+                cardBodyDiv.append(cardTitle, cardIcon, cardTemp, cardWind, cardHumid);
+                weatherCard.appendChild(cardBodyDiv);
+                rowDiv.appendChild(weatherCard);
             };
         };
     };
-}
+};
 
+// TODO search eventlistener to pull API data
+var searchButton = document.querySelector('#search-button');
+var searchInput = document.querySelector('#search-input');
+var searchDiv = document.querySelector('#input-div');
+var todaySection = document.querySelector('#today');
+var forecastSection = document.querySelector('#forecast');
+var historySection = document.querySelector('#history');
+
+searchButton.addEventListener("click", fetchGeocodeForecast);
 
 
 // TODO create a button function with every searched city
+function createCityButton(cityName) {
+    var cityButton = document.createElement('button');
+    cityButton.textContent = cityName;
+    cityButton.setAttribute("class", "city-buttons");
+    cityButton.setAttribute("data-city", cityName);
+    historySe   ction.append(cityButton);
+};
+
 
 // TODO store buttons and their data to local storage
+
